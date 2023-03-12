63cbb2de8d3608275da84201c4de1b81d6f28cf6
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-12 21:52:10 +0530

    receive code from esp

--- server.js
+++ server.js
@@ -6,26 +6,29 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 4000 }); // websocket server port
 app.use(express.json());
 app.use(cors());
-app.use('/api/v1',routes);
+app.use('/api/v1', routes);
 
 global.CurrState = 0
 
 // When a new client connects to the server
 wss.on('connection', function connection(ws) {
-    console.log('New client connected');
-  
-    // Send data to the client every second
-    const interval = setInterval(function sendData() {
-    //   const data = { value: Math.random() }; // generate random data
-      console.log('Sending data:',CurrState);
-      ws.send(CurrState.toString()); // convert data to string and send
-    }, 2000);
-  
-    // When the client disconnects
-    ws.on('close', function close() {
-      console.log('Client disconnected');
-      clearInterval(interval); // stop sending data to the disconnected client
-    });
-  });
+  console.log('New client connected');
 
-app.listen(5000, ()=> console.log(`App live at port 5000`));
+  ws.on('message', function incoming(message) {
+    console.log('Received message:', message);
+  });
+  // Send data to the client every second
+  const interval = setInterval(function sendData() {
+    //   const data = { value: Math.random() }; // generate random data
+    // console.log('Sending data:', CurrState);
+    ws.send(CurrState.toString()); // convert data to string and send
+  }, 2000);
+
+  // When the client disconnects
+  ws.on('close', function close() {
+    console.log('Client disconnected');
+    clearInterval(interval); // stop sending data to the disconnected client
+  });
+});
+
+app.listen(5000, () => console.log(`App live at port 5000`));
