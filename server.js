6f6a722a7e879b6a2484ac25d9b658d067503f82
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-12 12:07:23 +0530

    logs done

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ wss.on('connection', function connection(ws) {
     // Send data to the client every second
     const interval = setInterval(function sendData() {
     //   const data = { value: Math.random() }; // generate random data
-    //   console.log('Sending data:',CurrState);
+      console.log('Sending data:',CurrState);
       ws.send(CurrState.toString()); // convert data to string and send
-    }, 700);
+    }, 1000);
   
     // When the client disconnects
     ws.on('close', function close() {
